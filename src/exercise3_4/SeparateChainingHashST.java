5ee7b39a6bcff3524ba7bdb455df2afc6ff963d9
OldDream <huangyn15@qq.com>
2017-05-16 13:32:24 +0800

    Change Eclipse environment from GBK to UTF-8

--- src/exercise3_4/SeparateChainingHashST.java
+++ src/exercise3_4/SeparateChainingHashST.java
@@ -3,52 +3,51 @@ package exercise3_4;
 import edu.princeton.cs.algs4.Queue;
 
 public class SeparateChainingHashST<Key, Value> {
-    private int N;    // the total number of key-value pairs.
-    private int M;    // the size of hashST
-    private ModSequentialSearchST<Key, Value>[] st;
+	private int N; // the total number of key-value pairs.
+	private int M; // the size of hashST
+	private ModSequentialSearchST<Key, Value>[] st;
 
-    public SeparateChainingHashST() {
-        this(997);
-    }
+	public SeparateChainingHashST() {
+		this(997);
+	}
 
-    public SeparateChainingHashST(int M) {
-        this.M = M;
-        st = (ModSequentialSearchST<Key, Value>[]) new ModSequentialSearchST[M];
-        for (int i = 0; i < M; i++)
-            st[i] = new ModSequentialSearchST();
-    }
+	public SeparateChainingHashST(int M) {
+		this.M = M;
+		st = (ModSequentialSearchST<Key, Value>[]) new ModSequentialSearchST[M];
+		for (int i = 0; i < M; i++)
+			st[i] = new ModSequentialSearchST();
+	}
 
-    private int hash(Key key) {
-        return (key.hashCode() & 0x7fffffff) % M;    // 消除符号位并hash成大于0，小于M的整数
-    }
+	private int hash(Key key) {
+		return (key.hashCode() & 0x7fffffff) % M; // 消除符号位并hash成大于0，小于M的整数
+	}
 
-    public Value get(Key key) {
-        return (Value) st[hash(key)].get(key);
-    }
-    
-    public void put(Key key, Value val) {
-        st[hash(key)].put(key, val, N);
-        N++;
-    }
-    
-    public void delAbove(int k) {
-        for (int i = 0; i < M; i++) {
-            for (Key key : st[i].keys()) {
-                if (st[i].getTotalNum(key) > k) {
-                    st[i].delete(key);
-                }
-            }
-        }
-    }
+	public Value get(Key key) {
+		return (Value) st[hash(key)].get(key);
+	}
 
-    public Iterable<Key> keys() {
-        Queue<Key> qe = new Queue<>();
-        for (int i = 0; i < M; i++) {
-            for (Key key : st[i].keys()) {
-                qe.enqueue(key);
-            }
-        }
-        
-        return qe;
-    }
+	public void put(Key key, Value val) {
+		st[hash(key)].put(key, val, N);
+		N++;
+	}
+
+	public void delAbove(int k) {
+		for (int i = 0; i < M; i++) {
+			for (Key key : st[i].keys()) {
+				if (st[i].getTotalNum(key) > k) {
+					st[i].delete(key);
+				}
+			}
+		}
+	}
+
+	public Iterable<Key> keys() {
+		Queue<Key> qe = new Queue<>();
+		for (int i = 0; i < M; i++) {
+			for (Key key : st[i].keys()) {
+				qe.enqueue(key);
+			}
+		}
+		return qe;
+	}
 }
