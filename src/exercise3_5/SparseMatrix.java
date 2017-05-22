9362c2490fc502ec62358a8968e7b14373c3ee0d
OldDream <huangyn15@qq.com>
2017-05-22 11:23:38 +0800

    add new codes in 3-5-23

--- src/exercise3_5/SparseMatrix.java
+++ src/exercise3_5/SparseMatrix.java
@@ -4,15 +4,17 @@ import edu.princeton.cs.algs4.LinearProbingHashST;
 import edu.princeton.cs.algs4.StdOut;
 
 // Type is the Object in Matrix.
-public class SparseMatrix<Type> {
+public class SparseMatrix {
 
-    private LinearProbingHashST<Integer, Type>[] rowAl;
+    private LinearProbingHashST<Integer, Double>[] rowAl;
+    private int size;
 
     @SuppressWarnings("unchecked")
-    public SparseMatrix(boolean rowOrCol, int size, int indexOfV, LinearProbingHashST<Integer, Type> vector) {
+    public SparseMatrix(boolean rowOrCol, int size, int indexOfV, LinearProbingHashST<Integer, Double> vector) {
         // rowOrCol == true, vector is row, otherwise,it would be column.
         // indexOfV [0, size - 1]
-        rowAl = (LinearProbingHashST<Integer, Type>[]) new Object[size];
+        this.size = size;
+        rowAl = (LinearProbingHashST<Integer, Double>[]) new Object[size];
         if (indexOfV < 0 || indexOfV >= size) {
             StdOut.println("Wrong index.");
             System.exit(0);
@@ -23,10 +25,38 @@ public class SparseMatrix<Type> {
         }
         else {    // vector is column.
             for (Integer index : vector.keys()) {
-                rowAl[index].put(indexOfV,  vector.get(index));
+                rowAl[index].put(indexOfV, vector.get(index));
             }
         }
     }
-    
-    
+
+    public void add(SparseMatrix that) {
+        if (that.size() != size) {
+            StdOut.println("Wrong size.");
+            System.exit(0);
+        }
+        for (int i = 0; i < size; i++) {
+            // rowAl[i] is the row we are going to deal with.
+            for (Integer index : that.getRow(i).keys()) {
+                if (this.getRow(i).contains(index)) {
+                    Double val = this.getRow(i).get(index);
+                    val += that.getRow(i).get(index);
+                    this.getRow(i).put(index, val);
+                }
+                else {
+                    Double val = that.getRow(i).get(index);
+                    this.getRow(i).put(index, val);
+                }
+            }
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public LinearProbingHashST<Integer, Double> getRow(int index) {
+        return rowAl[index];
+    }
+
 }
