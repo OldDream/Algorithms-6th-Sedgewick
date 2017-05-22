f6741685517704b000a63dfcffca7f027db2da27
OldDream <huangyn15@qq.com>
2017-05-22 13:02:15 +0800

    finish 3-5-12

--- src/exercise3_5/SparseMatrix.java
+++ src/exercise3_5/SparseMatrix.java
@@ -29,10 +29,16 @@ public class SparseMatrix {
             }
         }
     }
+    
+    @SuppressWarnings("unchecked")
+    public SparseMatrix(int size) {
+        this.size = size;
+        rowAl = (LinearProbingHashST<Integer, Double>[]) new Object[size];
+    }
 
     public void add(SparseMatrix that) {
         if (that.size() != size) {
-            StdOut.println("Wrong size.");
+            StdOut.println("Two matrixs have different size");
             System.exit(0);
         }
         for (int i = 0; i < size; i++) {
@@ -49,6 +55,36 @@ public class SparseMatrix {
                 }
             }
         }
+    }
+    
+    //this multiply that = temp
+    public SparseMatrix multiply(SparseMatrix that) {
+        if (that.size() != size) {
+            StdOut.println("Two matrixs have different size.");
+            System.exit(0);
+        }
+        
+        SparseMatrix temp = new SparseMatrix(size);
+        for (int i = 0; i < size; i++) {    // i is the index of Row
+            for (int j = 0; j < size; j++) {    // j is the index of Column
+                LinearProbingHashST<Integer, Double> row = this.rowAl[i];
+                LinearProbingHashST<Integer, Double> col = new LinearProbingHashST<>();
+                // build a column vector.
+                for (int k = 0; k < size; k++) {    // k is a temporary row index.
+                    LinearProbingHashST<Integer, Double> tempRow = that.getRow(k);
+                    if (tempRow.contains(j))
+                        col.put(j, tempRow.get(j));
+                }
+                // multiply two vector.
+                double sum = 0;
+                for (int k : row.keys()) {
+                  sum += row.get(k) * col.get(k);
+                } 
+                // put the sum into matrix
+                temp.getRow(i).put(j, sum);
+            }
+        }
+        return temp;
     }
 
     public int size() {
