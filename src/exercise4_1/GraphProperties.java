35d5cb33f1ebdae9700a6b196cb4ec2e2a57cc8f
OldDream <huangyn15@qq.com>
2017-05-31 17:01:05 +0800

    add exercise 4_1_16

---
+++ src/exercise4_1/GraphProperties.java
@@ -0,0 +1,106 @@
+package exercise4_1;
+
+public class GraphProperties {
+    private boolean[] marked;
+    private int count;
+    private Graph g;
+    private int[] diameter;
+
+    public GraphProperties(Graph g) throws Exception {
+        this.g = g;
+        marked = new boolean[g.V()];
+        for (int i = 0; i < g.V(); i++) {
+            if (!marked[i]) {
+                dfs(g, i);
+                count++;
+            }
+        }
+
+        if (count < g.V())
+            throw (new Exception("The Graph is not connected."));
+    }
+
+    private void dfs(Graph g, int v) {
+        marked[v] = true;
+        for (int w : g.adj(v)) {
+            if (!marked[w]) {
+                dfs(g, w);
+                count++;
+            }
+        }
+    }
+
+    public int eccentricity(int v) {
+        int[] depthCounter = new int[g.V()];
+        for (int i = 0; i < depthCounter.length; i++) {
+            depthCounter[i] = -1;
+        }
+
+        depthCounter[v] = 0;
+        for (int w : g.adj(v)) {
+            depthCounter[w] = depthCounter[v] + 1;
+            eccentricity(w, depthCounter);
+        }
+
+        int max = 0;
+        for (int i = 0; i < depthCounter.length; i++) {
+            if (depthCounter[i] > max)
+                max = depthCounter[i];
+        }
+
+        return max;
+    }
+
+    private void eccentricity(int v, int[] depthCounter) {
+        for (int w : g.adj(v)) {
+            if (depthCounter[w] != -1)
+                continue;
+            depthCounter[w] = depthCounter[v] + 1;
+            eccentricity(w, depthCounter);
+        }
+    }
+
+    public int diameter() {
+        if (diameter == null) {
+            diameter = new int[g.V()];
+            for (int i = 0; i < g.V(); i++) {
+                diameter[i] = eccentricity(i);
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < diameter.length; i++) {
+            if (diameter[i] > max)
+                max = diameter[i];
+        }
+
+        return max;
+    }
+
+    public int radius() {
+        if (diameter == null) {
+            diameter = new int[g.V()];
+            for (int i = 0; i < g.V(); i++) {
+                diameter[i] = eccentricity(i);
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < diameter.length; i++) {
+            if (diameter[i] < min)
+                min = diameter[i];
+        }
+
+        return min;
+    }
+    
+    public int center() {
+        int radius = radius();
+        for (int i = 0; i < g.V(); i++) {
+            int eccI = eccentricity(i);
+            if (eccI == radius)
+                return i;
+        }
+        
+        return -1;    // there is no center;
+    }
+
+}
