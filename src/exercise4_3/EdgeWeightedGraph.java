d1d4171d895a36fbca7f4db8b8e1d7a8b9607c4a
OldDream <huangyn15@qq.com>
2017-06-30 10:42:56 +0800

    new exercise in 4.3

---
+++ src/exercise4_3/EdgeWeightedGraph.java
@@ -0,0 +1,58 @@
+package exercise4_3;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+
+public class EdgeWeightedGraph {
+    private final int V;    // the sum of vertexes
+    private int E;    // the sum of Edges
+    private Bag<Edge>[] adj;    // the adjacent list
+
+    @SuppressWarnings("unchecked")
+    public EdgeWeightedGraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Edge>[]) new Bag[V];
+        for (int i = 0; i < V; i++)
+            adj[i] = new Bag<Edge>();
+    }
+    
+    public EdgeWeightedGraph(In in) {
+        this(in.readInt());
+        this.E = in.readInt();
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            double weight = in.readDouble();
+            Edge e = new Edge(v, w, weight);
+            addEdge(e);
+        }
+    }
+    
+    public int V() {
+        return V;
+    }
+    
+    public int E() {
+        return E;
+    }
+    
+    public void addEdge(Edge e) {
+        int v = e.either();
+        int w = e.other(v);
+        adj[v].add(e);
+        adj[w].add(e);
+        E++;
+    }
+    
+    public Iterable<Edge> edges() {
+        Bag<Edge> b = new Bag<Edge>();
+        for (int v= 0; v < V; v++) {
+            for (Edge e : adj[v]) {
+                if (e.other(v) > v)
+                    b.add(e);
+            }
+        }
+       return b;
+    }
+}
