7dc78e704a77260764203bcfee77dab18e727c67
OldDream <huangyn15@qq.com>
2017-07-23 21:19:03 +0800

    finish 4.4.2

--- src/exercise4_4/EdgeWeightedDigraph.java
+++ src/exercise4_4/EdgeWeightedDigraph.java
@@ -5,6 +5,7 @@ import edu.princeton.cs.algs4.DirectedEdge;
 import edu.princeton.cs.algs4.In;
 
 public class EdgeWeightedDigraph {
+    private static final String NEWLINE = System.getProperty("line.separator");
     private final int V;
     private int E;
     private Bag<DirectedEdge>[] adj;
@@ -16,9 +17,73 @@ public class EdgeWeightedDigraph {
             adj[i] = new Bag<DirectedEdge>();
         }
     }
-    
+    /**  
+     * Initializes an edge-weighted digraph from the specified input stream.
+     * The format is the number of vertices <em>V</em>,
+     * followed by the number of edges <em>E</em>,
+     * followed by <em>E</em> pairs of vertices and edge weights,
+     * with each entry separated by whitespace.
+     *
+     * @param  in the input stream
+     * @throws IllegalArgumentException if the endpoints of any edge are not in prescribed range
+     * @throws IllegalArgumentException if the number of vertices or edges is negative
+     */
     public EdgeWeightedDigraph(In in) {
         this(in.readInt());
+        this.E = in.readInt();
+        if (E < 0) throw new IllegalArgumentException("Number of edges must be nonnegative");
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            validateVertex(v);
+            validateVertex(w);
+            double weight = in.readDouble();
+            addEdge(new DirectedEdge(v, w, weight));
+        }
+    }
+    
+    public void addEdge(DirectedEdge e) {
+        adj[e.from()].add(e);
+        E++;
+    }
+    
+    public int V() {
+        return V;
+    }
+    
+    public int E() {
+        return E;
+    }
+    
+    public Iterable<DirectedEdge> adj(int v) {
+        return adj[v];
+    }
+    
+    public Iterable<DirectedEdge> edges() {
+        Bag<DirectedEdge> bag = new Bag<DirectedEdge>();
+        for (int i = 0; i < V; i++) {
+            for (DirectedEdge e : adj[i])
+                bag.add(e);
+        }
         
+        return bag;
+    }
+    
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " " + E + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (DirectedEdge e : adj[v]) {
+                s.append(e + "  ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+    
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
     }
 }
